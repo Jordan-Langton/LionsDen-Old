070e77160f56a825aa04cdc1875b5558aab22cdc
PostmanPat123 <42339292+PostmanPat123@users.noreply.github.com>
2020-08-15 17:16:44 +0200

    got the dummy orders to show on the dash

--- public/app.state.js
+++ public/app.state.js
@@ -1,18 +1,18 @@
-
+import liveOrders from "./orders";
 
 export default {
 
   "model": {
-    
+    ...liveOrders.model
   },
   "getters": {
-    
+    ...liveOrders.getters
   },
   "mutations": {
-
+    ...liveOrders.mutations
   },
   "actions": {
-    
+    ...liveOrders.actions
   },
 
 };
--- reusableCode/sideNavbar.js
+++ reusableCode/sideNavbar.js
@@ -1,4 +1,4 @@
-export const sideNavbar = {   
+export const sideNavbar = {
     data: {
         test: ''
     },
